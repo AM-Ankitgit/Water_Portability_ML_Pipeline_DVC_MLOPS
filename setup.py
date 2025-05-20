378b19e9833b9bd46c05312a66380d77496fb359
AM-Ankitgit <mahalleankit007@gmail.com>
2025-05-20 09:50:13 +0530

    all file uploaded

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='it is water portability project which focuses on proper machine learning cicd pipeline which includes various DevOps tools and technology',
+    author='Ankit Mahalle',
+    license='MIT',
+)
